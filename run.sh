726f4a2698a15807ec6ddd85928857fd667b5ede
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-09-08 18:37:16 -0400

    Add run script to execute example DGMs

---
+++ run.sh
@@ -0,0 +1,5 @@
+./dgmc ../examples/hello.dgm -o hello --run
+./dgmc ../examples/safe_add.dgm -o safe_add --run
+./dgmc ../examples/tuple_test.dgm -o tuple_test --run
+./dgmc ../examples/list_test.dgm -o list_test --run
+./dgmc ../examples/parallel.dgm -o parallel --run
